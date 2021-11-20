b3a875fa604fa03020901407a632301c0c7f276a
Septonious <83358692+Septonious@users.noreply.github.com>
2021-11-20 19:32:48 +0300

    amogus

--- shaders/program/composite1.glsl
+++ shaders/program/composite1.glsl
@@ -71,6 +71,8 @@ float GetLuminance(vec3 color) {
 #include "/lib/color/waterColor.glsl"
 #include "/lib/util/dither.glsl"
 
+#if defined OVERWORLD && PERBIOME_LIGHTSHAFTS
+
 float InterleavedGradientNoiseVL() {
 	float n = 52.9829189 * fract(0.06711056 * gl_FragCoord.x + 0.00583715 * gl_FragCoord.y);
 	return fract(n + frameCounter / 6.0);
