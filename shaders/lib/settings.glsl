7ff7e9f2f6b4e3a43294a828b1ab79ad77101cc0
Septonious <83358692+Septonious@users.noreply.github.com>
2021-11-20 22:23:26 +0300

    Update settings.glsl

--- shaders/lib/settings.glsl
+++ shaders/lib/settings.glsl
@@ -352,6 +352,8 @@ https://bitslablab.com
   const int shadowMapResolution = 2048; //[512 1024 2048 3072 4096 8192]
   const float shadowDistance = 512.0; //[128.0 256.0 512.0 1024.0]
   const float shadowMapBias = 1.0 - 25.6 / shadowDistance;
+  #define SHADOW_COLOR
+  #define SHADOW_FILTER
   const float sunPathRotation = -20.0; //[-60.0 -55.0 -50.0 -45.0 -40.0 -35.0 -30.0 -25.0 -20.0 -15.0 -10.0 -5.0 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0]
   #define SHADOW_PIXEL 0 //[0 16 32 64 128]
   #define AO
