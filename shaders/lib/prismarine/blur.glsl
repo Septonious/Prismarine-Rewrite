95141a18708830846355e6a10abea5b8e6d7058b
Septonious <83358692+Septonious@users.noreply.github.com>
2021-12-25 14:43:30 +0300

    Update blur.glsl

--- shaders/lib/prismarine/blur.glsl
+++ shaders/lib/prismarine/blur.glsl
@@ -38,48 +38,8 @@ vec3 BoxBlur(sampler2D colortex, float strength, vec2 coord) {
 
 	for(int j = 0; j <= 32; j++){
 		vec2 offset = BlurOffsets[j] * strength * vec2(1.0 / aspectRatio, 1.0);
-		blur += texture2DLod(colortex, texCoord + offset, log2(viewHeight)).rgb;
+		blur += texture2DLod(colortex, texCoord + offset, 1.0).rgb;
 	}
 
 	return blur / 32.0;
 }
-
-/*
-#define INV_SQRT_OF_2PI 0.39894228040143267793994605993439
-#define INV_PI 0.31830988618379067153776752674503
-
-vec3 smartDeNoise(sampler2D tex, vec2 uv, float sigma, float kSigma, float threshold){
-    float radius = round(kSigma * sigma);
-    float radQ = radius * radius;
-    
-    float invSigmaQx2 = 0.5 / (sigma * sigma);
-    float invSigmaQx2PI = INV_PI * invSigmaQx2;
-    
-    float invThresholdSqx2 = 0.5 / (threshold * threshold);
-    float invThresholdSqrt2PI = INV_SQRT_OF_2PI / threshold;
-    
-    vec4 centrPx = texture(tex,uv);
-    
-    float zBuff = 0.0;
-    vec4 aBuff = vec4(0.0);
-    vec2 size = vec2(textureSize(tex, 0));
-    
-    for(float x= -radius; x <= radius; x++) {
-        float pt = sqrt(radQ - x * x);
-        for(float y= -pt; y <= pt; y++) {
-            vec2 d = vec2(x, y);
-
-            float blurFactor = exp(-dot(d , d) * invSigmaQx2) * invSigmaQx2PI; 
-            
-            vec4 walkPx = texture(tex, uv + d / size);
-
-            vec4 dC = walkPx-centrPx;
-            float deltaFactor = exp( -dot(dC, dC) * invThresholdSqx2) * invThresholdSqrt2PI * blurFactor;
-                                 
-            zBuff += deltaFactor;
-            aBuff += deltaFactor * walkPx;
-        }
-    }
-    return (aBuff / zBuff).rgb;
-}
-*/
